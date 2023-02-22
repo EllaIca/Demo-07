13cd31c3c4c8a4b24390a807f15f7d6725feb896
EllaIca <114512576+EllaIca@users.noreply.github.com>
2023-02-21 21:05:41 -0800

    Making login page

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -1,14 +1,15 @@
 //----------------------------------------
 //  Your web app's Firebase configuration
 //----------------------------------------
-var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyD-y8ZUQ8FVpFBtvD-Tcdel4bjDJrGroUA",
+    authDomain: "comp1800-7f5a9.firebaseapp.com",
+    projectId: "comp1800-7f5a9",
+    storageBucket: "comp1800-7f5a9.appspot.com",
+    messagingSenderId: "538252558452",
+    appId: "1:538252558452:web:3ddda2897fe8d0a8e3d121",
+    measurementId: "G-FENY8EW1GN"
+  };
 
 //--------------------------------------------
 // initialize the Firebase app
